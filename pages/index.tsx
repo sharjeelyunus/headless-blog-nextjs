ed5b1d6a02df77c5135d1970f20f8b2a8ceeba66
sharjeelyunus <sharjeel924@gmail.com>
2021-11-19 13:44:55 +0500

    👌 IMPROVE: Add Images from API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import styles from '../styles/Home.module.scss';
 import Link from 'next/link';
+import Image from 'next/image';
 const { BLOG_URL, CONTENT_API_KEY } = process.env;
 
 type Post = {
   title: string;
   slug: string;
+  feature_image: string;
 };
 
 async function getPosts() {
   const res = await fetch(
-    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt`
+    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt,feature_image`
   ).then((res) => res.json());
 
   const posts = res.posts;
@@ -31,17 +33,32 @@ const Home: React.FC<{ posts: Post[] }> = (props) => {
   return (
     <div className={styles.container}>
       <h1>Hello to my Blog</h1>
-      <ul>
+      <div className={styles.postList}>
         {posts.map((post) => {
           return (
-            <li key={post.slug} className={styles.postitem}>
-              <Link href='/post/[slug]' as={`/post/${post.slug}`}>
-                <a href=''>{post.title}</a>
+            <div key={post.slug} className={styles.postitem}>
+              <Link
+                href='/post/[slug]'
+                as={`/post/${post.slug}`}
+                passHref={true}
+              >
+                <div className={styles.posts}>
+                  <Image
+                    loader={() => post.feature_image}
+                    src={post.feature_image}
+                    layout='responsive'
+                    objectFit='cover'
+                    width={6}
+                    height={4}
+                    alt=''
+                  />
+                  <a>{post.title}</a>
+                </div>
               </Link>
-            </li>
+            </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 };
